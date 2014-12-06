75dc04d7d1257ddd6615e66d4ffb2e624508fff3
ianzapolsky <ianzapolsky@gmail.com>
2014-12-06 12:55:31 -0500

    okayy, real results now

--- src/parse.py
+++ src/parse.py
@@ -1,5 +1,6 @@
 
 f = open('tournament_summary.csv', 'r')
+lines = f.readlines()
 
 groups = {'group1': 0,
           'group2': 0,
@@ -11,13 +12,32 @@ groups = {'group1': 0,
           'group8': 0,
           'group9': 0}
 
-games = -1
+wins  = 0
+games = 0
 
-for line in f:
-  games += 1
-  if games == 0:
-    continue
-  groups[line.split(',')[4]] += 1
-    
-print groups
+LAND  = 6
+WATER = 7
+POSTS = 8
+
+for i in range(len(lines) - 4):
+  if i % 4 == 0:
+    if lines[i].find('group4') == -1:
+      i += 4
+    else:
+      maxscore = 0
+      ourscore = 0
+      for j in range(1, 5):
+        if lines[i+j].split(',')[4] == 'group4':
+          ourscore = lines[i+j].split(',')[WATER]
+        else:
+          if maxscore < lines[i+j].split(',')[WATER]:
+            maxscore = lines[i+j].split(',')[WATER]
+      if maxscore == ourscore:
+        wins += 1
+      games += 1  
+          
+print 'out of '+str(games)+' we won '+str(wins)
+        
+        
+  
 
